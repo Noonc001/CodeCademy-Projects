16ed11cb009191889ef46240b46dc4902681f49d
Noonc001 <64410321+Noonc001@users.noreply.github.com>
2020-08-18 16:33:58 +0100

    Update Magic8Ball.java

--- Java Magic 8 Ball/Magic8Ball.java
+++ Java Magic 8 Ball/Magic8Ball.java
@@ -2,7 +2,7 @@
 
 public class Magic8 {
 
-    //Generated random number, Static keyword is needed to save the value when utilising the main method.
+    //Generated random number, Static keyword is used to save the value when deploying the random number in the main method.
   
     static int random = (int) (Math.random() * 8 + 1);
 
